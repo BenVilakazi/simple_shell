f272362d49ff2e82401917992db9546e6bca9e54
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-10-02 21:31:02 +0000

    more env

--- environ.c
+++ environ.c
@@ -3,18 +3,15 @@
  * Auth: Ben Vilakazi
  *       Thapelo M
  */
-
 #include "shell.h"
 
 char **_copyenv(void);
 void free_env(void);
 char **_getenv(const char *var);
-
 /**
- * _copyenv - Creates a copy of the environment.
- *
+ * _copyenv - copies of the environment.
  * Return: If an error occurs - NULL.
- *         O/w - a double pointer to the new copy.
+ *         O/w - a double ptr to the new copy.
  */
 char **_copyenv(void)
 {
@@ -46,7 +43,6 @@ char **_copyenv(void)
 
 	return (new_environ);
 }
-
 /**
  * free_env - Frees the the environment copy.
  */
@@ -58,13 +54,11 @@ void free_env(void)
 		free(environ[index]);
 	free(environ);
 }
-
 /**
  * _getenv - Gets an environmental variable from the PATH.
  * @var: The name of the environmental variable to get from PATH.
- *
- * Return: If the environmental variable does not exist - NULL.
- *         Otherwise - a pointer to the environmental variable.
+ * Return: If the environmental var does not exist - NULL.
+ *         else - a ptr to the environmental variable.
  */
 char **_getenv(const char *var)
 {
