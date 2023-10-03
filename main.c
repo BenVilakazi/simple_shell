580317fb5ecffa268c8c6f704195ec17a6c74102
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-10-03 15:52:39 +0000

    main

--- main.c
+++ main.c
@@ -3,14 +3,12 @@
  * Auth: Ben Vilakazi
  *       Thapelo M
  */
-
 #include "shell.h"
 
 void sig_handler(int sig);
 int execute(char **args, char **front);
-
 /**
- * sig_handler - Prints a new prompt upon a signal.
+ * sig_handler - prints a new prompt when signalled.
  * @sig: The signal.
  */
 void sig_handler(int sig)
@@ -21,13 +19,12 @@ void sig_handler(int sig)
 	signal(SIGINT, sig_handler);
 	write(STDIN_FILENO, new_prompt, 3);
 }
-
 /**
- * execute - Executes a command in a child process.
- * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
- * Return: If an error occurs - a corresponding error code.
- *         O/w - The exit value of the last executed command.
+ * execute - executes a command in a child process.
+ * @args: args vector.
+ * @front: A double ptr to the start of args.
+ * Return: If an err occurs - a corresponding error code.
+ *         else - The exit value of the last executed command.
  */
 int execute(char **args, char **front)
 {
@@ -78,11 +75,10 @@ int execute(char **args, char **front)
 		free(command);
 	return (ret);
 }
-
 /**
  * main - Runs a simple UNIX command interpreter.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @argc: args count.
+ * @argv: args vector.
  * Return: The return value of the last executed command.
  */
 int main(int argc, char *argv[])
